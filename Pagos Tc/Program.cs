5ba8e7256d3fbf14b6baf531bb00e408c7a5e410
LadyBetancourt <lady_betancourt24@unikino.edu.mx>
2024-10-02 07:57:15 -0700

    Agregar archivos de proyecto.

---
+++ Pagos Tc/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Pagos_Tc
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string codigo = DateTime.Now.ToString("yymmss");
+            Console.WriteLine(codigo);
+
+            Console.WriteLine("Introduce tu codigo de verificación");
+            string codigoEscrito = Console.ReadLine();
+
+            if (codigoEscrito == codigo)
+            {
+                Console.WriteLine("ok");
+
+            }
+            else
+            {
+
+
+        }
+    }
+}
